b98b12245cc934a7ea79dcb4927b12d02e51f1d1
DoomyDwyer <doomy@puscii.nl>
2021-08-19 22:47:26 +0100

    Phaser RackAFX project created

---
+++ Phaser/PluginKernel/plugindescription.h
@@ -0,0 +1,77 @@
+// --- CMAKE generated variables for your plugin
+
+#include "pluginstructures.h"
+
+#ifndef _plugindescription_h
+#define _plugindescription_h
+
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+#define AU_COCOA_VIEWFACTORY_STRING STR(AU_COCOA_VIEWFACTORY_NAME)
+#define AU_COCOA_VIEW_STRING STR(AU_COCOA_VIEW_NAME)
+
+// --- AU Plugin Cocoa View Names (flat namespace)
+#define AU_COCOA_VIEWFACTORY_NAME AUCocoaViewFactory_B645FD1893FC4C60995A8CF29FD9D925
+#define AU_COCOA_VIEW_NAME AUCocoaView_B645FD1893FC4C60995A8CF29FD9D925
+
+// --- BUNDLE IDs (MacOS Only)
+const char* kAAXBundleID = "doomsvillesoundscapes.aax.Phaser.bundleID";
+const char* kAUBundleID = "doomsvillesoundscapes.au.Phaser.bundleID";
+const char* kVST3BundleID = "doomsvillesoundscapes.vst3.Phaser.bundleID";
+
+// --- Plugin Names
+const char* kPluginName = "Phaser";
+const char* kShortPluginName = "Phaser";
+const char* kAUBundleName = "Phaser";
+const char* kAAXBundleName = "Phaser";
+const char* kVSTBundleName = "Phaser";
+
+// --- bundle name helper
+inline static const char* getPluginDescBundleName()
+{
+#ifdef AUPLUGIN
+	return kAUBundleName;
+#endif
+
+#ifdef AAXPLUGIN
+	return kAAXBundleName;
+#endif
+
+#ifdef VSTPLUGIN
+	return kVSTBundleName;
+#endif
+
+	// --- should never get here
+	return kPluginName;
+}
+
+// --- Plugin Type
+const pluginType kPluginType = pluginType::kFXPlugin;
+
+// --- VST3 UUID
+const char* kVSTFUID = "{B645FD18-93FC-4C60-995A-8CF29FD9D925}";
+
+// --- 4-char codes
+const int32_t kFourCharCode = 'PHSR';
+const int32_t kAAXProductID = 'PHSR';
+const int32_t kManufacturerID = 'DOOM';
+
+// --- Vendor information
+const char* kVendorName = "Doomsville Soundscapes";
+const char* kVendorURL = "github.com/DoomyDwyer";
+const char* kVendorEmail = "doomy@puscii.nl";
+
+// --- Plugin Options
+const bool kProcessFrames = true;
+const uint32_t kBlockSize = DEFAULT_AUDIO_BLOCK_SIZE;
+const bool kWantSidechain = true;
+const uint32_t kLatencyInSamples = 0;
+const double kTailTimeMsec = 0.000;
+const bool kVSTInfiniteTail = false;
+const bool kVSTSAA = false;
+const uint32_t kVST3SAAGranularity = 1;
+const uint32_t kAAXCategory = 0;
+
+#endif
+
+
