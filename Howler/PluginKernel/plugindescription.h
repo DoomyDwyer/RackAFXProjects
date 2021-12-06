6fec0f40ad2a967fa9a010882c84bb0c2768d133
DoomyDwyer <doomy@puscii.nl>
2021-12-06 23:51:49 +0000

    Howler (Tube Sccreamer Analog Modeling - DSP code by Eric Tarr) - RackAFX project implemented and working as RackAFX prototype (no GUI yet)

---
+++ Howler/PluginKernel/plugindescription.h
@@ -0,0 +1,77 @@
+// --- CMAKE generated variables for your plugin
+
+#include "pluginstructures.h"
+
+#ifndef _plugindescription_h
+#define _plugindescription_h
+
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+#define AU_COCOA_VIEWFACTORY_STRING STR(AU_COCOA_VIEWFACTORY_NAME)
+#define AU_COCOA_VIEW_STRING STR(AU_COCOA_VIEW_NAME)
+
+// --- AU Plugin Cocoa View Names (flat namespace)
+#define AU_COCOA_VIEWFACTORY_NAME AUCocoaViewFactory_871889849A0E48FE8CC81800F6ABD8D8
+#define AU_COCOA_VIEW_NAME AUCocoaView_871889849A0E48FE8CC81800F6ABD8D8
+
+// --- BUNDLE IDs (MacOS Only)
+const char* kAAXBundleID = "doomsvillesoundscapes.aax.Howler.bundleID";
+const char* kAUBundleID = "doomsvillesoundscapes.au.Howler.bundleID";
+const char* kVST3BundleID = "doomsvillesoundscapes.vst3.Howler.bundleID";
+
+// --- Plugin Names
+const char* kPluginName = "Howler";
+const char* kShortPluginName = "Howler";
+const char* kAUBundleName = "Howler";
+const char* kAAXBundleName = "Howler";
+const char* kVSTBundleName = "Howler";
+
+// --- bundle name helper
+inline static const char* getPluginDescBundleName()
+{
+#ifdef AUPLUGIN
+	return kAUBundleName;
+#endif
+
+#ifdef AAXPLUGIN
+	return kAAXBundleName;
+#endif
+
+#ifdef VSTPLUGIN
+	return kVSTBundleName;
+#endif
+
+	// --- should never get here
+	return kPluginName;
+}
+
+// --- Plugin Type
+const pluginType kPluginType = pluginType::kFXPlugin;
+
+// --- VST3 UUID
+const char* kVSTFUID = "{87188984-9A0E-48FE-8CC8-1800F6ABD8D8}";
+
+// --- 4-char codes
+const int32_t kFourCharCode = 'HOWL';
+const int32_t kAAXProductID = 'HOWL';
+const int32_t kManufacturerID = 'DOOM';
+
+// --- Vendor information
+const char* kVendorName = "Doomsville Soundscapes";
+const char* kVendorURL = "github.com/DoomyDwyer";
+const char* kVendorEmail = "doomy@puscii.nl";
+
+// --- Plugin Options
+const bool kProcessFrames = true;
+const uint32_t kBlockSize = DEFAULT_AUDIO_BLOCK_SIZE;
+const bool kWantSidechain = true;
+const uint32_t kLatencyInSamples = 0;
+const double kTailTimeMsec = 0.000;
+const bool kVSTInfiniteTail = false;
+const bool kVSTSAA = false;
+const uint32_t kVST3SAAGranularity = 1;
+const uint32_t kAAXCategory = 0;
+
+#endif
+
+
